b34d8db726bceddd090c66042022f32cffe84b4d
Victor Yevitchenko <cheer4engineer@gmail.com>
2022-09-18 23:14:39 +0500

    Refactored ssl certificate generating to be suitable for Android.

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ For docker image wiznote/wizserver adds https support, docker expose 443 port
 3. Start image `docker run --name wiz-ssl --restart=always -it -d -v ~/wizdata:/wiz/storage -v /etc/localtime:/etc/localtime -p 80:80 -p 9269:9269/udp -p 443:443 wiz-server-ssl`
 
 [Here details of Wiznote server image deploy](https://wiz.cn/zh-cn/docker)
+
+Install certificate for Linux:
+```
+sudo cp wiz-server.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+```
+
+Install certificate for Android:
+Deliver wiz-server.der.crt to the device. Install it by tap or Settings > Security & Location > Encryption & credentials > Install from SD Card > Downloads.\
+Verify it's installed by navigating to Settings > Security & Location > Encryption & credentials > Trusted Credentials > User.
--- build/gen_cert.sh
+++ build/gen_cert.sh
@@ -9,6 +9,8 @@ domain=wizexample.com
 rm -rf $out_dir
 mkdir $out_dir
 
-openssl req -new -newkey rsa:2048 -sha256 -nodes -out $out_dir/wiz-server.csr -keyout $out_dir/wiz-server.key -subj "/C=${country}/ST=${city}/L=${city}/O=${organization}/OU=Web Secutiry/CN=${domain}"
-
-openssl x509 -req -days 30 -in $out_dir/wiz-server.csr -signkey $out_dir/wiz-server.key -out $out_dir/wiz-server.crt
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -out $out_dir/wiz-ca.crt -keyout $out_dir/wiz-ca.key -subj "/C=${country}/ST=${city}/L=${city}/O=${organization}/OU=Web Secutiry/CN=${domain}"
+openssl genrsa -out $out_dir/wiz-server.key 2048
+openssl req -new -key $out_dir/wiz-server.key -out $out_dir/wiz-server.csr -config openssl.cnf
+openssl x509 -req -days 3650 -in $out_dir/wiz-server.csr -CA $out_dir/wiz-ca.crt -CAkey $out_dir/wiz-ca.key -CAcreateserial -extensions v3_req -extfile openssl.cnf -out $out_dir/wiz-server.crt
+openssl x509 -inform PEM -outform DER -in $out_dir/wiz-server.crt -out $out_dir/wiz-server.der.crt
